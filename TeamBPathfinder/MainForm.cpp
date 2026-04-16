2924399dc899c39d84e9e669a3c63a4a26c6721c
Kellen Andrews <kandre15@my.westga.edu>
2026-04-16 09:17:36 -0400

    Add project files.

---
+++ TeamBPathfinder/MainForm.cpp
@@ -0,0 +1,15 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace TeamBPathfinder;
+
+[STAThread]
+int main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MainForm());
+	return 0;
+}
+
