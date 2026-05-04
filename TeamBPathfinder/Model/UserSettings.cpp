d3029a58fb7a0cec236ef6fb751dfc9d1f16b3c2
jmirand3 <jmirand3@my.westga.edu>
2026-05-04 18:57:42 -0400

    Updated Usersettings with documentation and namespaces. Also went ahead and moved constants to the header.

--- TeamBPathfinder/Model/UserSettings.cpp
+++ TeamBPathfinder/Model/UserSettings.cpp
@@ -1,47 +1,46 @@
 #include "UserSettings.h"
 
-static const int DEFAULT_CELL_COLOR = (int)0xFFFFFFFF;
-static const int DEFAULT_NUMBER_COLOR = (int)0xFF3C3C3C;
-static const int DEFAULT_FIXED_CELL_COLOR = (int)0xFFDCDCDC;
-
-UserSettings::UserSettings()
+namespace Model
 {
-    resetToDefaults();
-}
+	UserSettings::UserSettings()
+	{
+		resetToDefaults();
+	}
 
-int UserSettings::getCellColor() const
-{
-    return this->cellColorRgb;
-}
+	int UserSettings::getCellColor() const
+	{
+		return this->cellColorRgb;
+	}
 
-int UserSettings::getNumberColor() const
-{
-    return this->numberColorRgb;
-}
+	int UserSettings::getNumberColor() const
+	{
+		return this->numberColorRgb;
+	}
 
-int UserSettings::getFixedCellColor() const
-{
-    return this->fixedCellColorRgb;
-}
+	int UserSettings::getFixedCellColor() const
+	{
+		return this->fixedCellColorRgb;
+	}
 
-void UserSettings::setCellColor(int rgb)
-{
-    this->cellColorRgb = rgb;
-}
+	void UserSettings::setCellColor(int rgb)
+	{
+		this->cellColorRgb = rgb;
+	}
 
-void UserSettings::setNumberColor(int rgb)
-{
-    this->numberColorRgb = rgb;
-}
+	void UserSettings::setNumberColor(int rgb)
+	{
+		this->numberColorRgb = rgb;
+	}
 
-void UserSettings::setFixedCellColor(int rgb)
-{
-    this->fixedCellColorRgb = rgb;
-}
+	void UserSettings::setFixedCellColor(int rgb)
+	{
+		this->fixedCellColorRgb = rgb;
+	}
 
-void UserSettings::resetToDefaults()
-{
-    this->cellColorRgb = DEFAULT_CELL_COLOR;
-    this->numberColorRgb = DEFAULT_NUMBER_COLOR;
-    this->fixedCellColorRgb = DEFAULT_FIXED_CELL_COLOR;
+	void UserSettings::resetToDefaults()
+	{
+		this->cellColorRgb = DEFAULT_CELL_COLOR;
+		this->numberColorRgb = DEFAULT_NUMBER_COLOR;
+		this->fixedCellColorRgb = DEFAULT_FIXED_CELL_COLOR;
+	}
 }
