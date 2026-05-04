3b619e45892377b20855a4004c49d2311f733362
jmirand3 <jmirand3@my.westga.edu>
2026-05-03 21:06:22 -0400

    Moved gamesnapshot to model folder

--- TeamBPathfinder/Model/GameSnapshot.cpp
+++ TeamBPathfinder/Model/GameSnapshot.cpp
@@ -1,21 +1,67 @@
-#pragma once
+#include "GameSnapshot.h"
 
-#include <vector>
-
-class GameSnapshot
+GameSnapshot::GameSnapshot()
 {
-private:
-    int currentPuzzleIndex;
-    std::vector<std::vector<std::vector<int>>> boards;
+    this->currentPuzzleIndex = 0;
+}
 
-public:
-    GameSnapshot();
-    GameSnapshot(int puzzleCount);
+GameSnapshot::GameSnapshot(int puzzleCount)
+{
+    this->currentPuzzleIndex = 0;
 
-    int getCurrentPuzzleIndex() const;
-    void setCurrentPuzzleIndex(int index);
+    this->boards.resize(
+        puzzleCount,
+        std::vector<std::vector<int>>(
+            8,
+            std::vector<int>(8, 0)
+        )
+    );
 
-    int getBoardCount() const;
-    int getValue(int puzzleIndex, int row, int col) const;
-    void setValue(int puzzleIndex, int row, int col, int value);
-};
+    this->solved.resize(puzzleCount, false);
+    this->seconds.resize(puzzleCount, 0);
+}
+
+int GameSnapshot::getCurrentPuzzleIndex() const
+{
+    return this->currentPuzzleIndex;
+}
+
+void GameSnapshot::setCurrentPuzzleIndex(int index)
+{
+    this->currentPuzzleIndex = index;
+}
+
+int GameSnapshot::getBoardCount() const
+{
+    return (int)this->boards.size();
+}
+
+int GameSnapshot::getValue(int puzzleIndex, int row, int col) const
+{
+    return this->boards[puzzleIndex][row][col];
+}
+
+void GameSnapshot::setValue(int puzzleIndex, int row, int col, int value)
+{
+    this->boards[puzzleIndex][row][col] = value;
+}
+
+bool GameSnapshot::getSolved(int puzzleIndex) const
+{
+    return this->solved[puzzleIndex];
+}
+
+void GameSnapshot::setSolved(int puzzleIndex, bool isSolved)
+{
+    this->solved[puzzleIndex] = isSolved;
+}
+
+int GameSnapshot::getSeconds(int puzzleIndex) const
+{
+    return this->seconds[puzzleIndex];
+}
+
+void GameSnapshot::setSeconds(int puzzleIndex, int sec)
+{
+    this->seconds[puzzleIndex] = sec;
+}
